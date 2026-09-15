11ca33c79b91ccc4d520b9f50c17af4e8d7c8b62
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-09-15 12:05:28 -0500

    Added rough footer element with team member names.

---
+++ src/component/Footer/footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p><span>Michael Obikwere</span> <br /> <span>Kailine Lima</span> <br /> <span>Juliet Azagba</span></p>
+    </footer>
+  );
+}
+
+export default Footer;
