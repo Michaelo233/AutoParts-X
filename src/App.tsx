ae5bdbf7fbbdc7b68de637f98dfc3fe490712dad
Klima2 <klima2@rrc.ca>
2026-09-16 18:19:18 -0500

    fix deleted unecessaery imports from app.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import Footer from './component/Footer/footer'
 import Header from './component/Header/header'
 import SellService from './component/Juliet/SellService'
 import RentAndBuy from './component/MichaelComponent/rentBuy'
-import Registration from './component/kailine_components/registration/register'
-import RecycleService from './component/kailine_components/recycle_service/recycle_service'
-
 
 function App() {
 
