15e95db649da1fd42cbb5814574f734915d45ae4
Klima2 <klima2@rrc.ca>
2026-09-23 09:46:55 -0500

    feat: added module.css and style.value, and created style footer and add the @copy.

--- src/component/Footer/footer.tsx
+++ src/component/Footer/footer.tsx
@@ -1,7 +1,14 @@
+import styles from "./footer.module.css";
+
 function Footer() {
   return (
-    <footer>
-      <p><span>Michael Obikwere</span> <br /> <span>Kailine Lima</span> <br /> <span>Juliet Azagba</span></p>
+    <footer className={styles.footerContainer}>
+      <p className={styles.footerText}>
+       <span>Michael Obikwere</span> <br />
+       <span>Kailine Lima</span> <br /> 
+       <span>Juliet Azagba</span>
+       </p>
+       <p className={styles.footerYear}>Copyright Pixell River Financial {new Date().getFullYear()}</p>
     </footer>
   );
 }
